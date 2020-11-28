99695377c955a9eeb3de071dcc8df262ff9489b6
ferizoozoo <farhadzohoorz@gmail.com>
2020-11-28 12:40:44 +0330

    Make the app create blocks with single thread executor

--- src/main/java/blockchain/App.java
+++ src/main/java/blockchain/App.java
@@ -1,14 +1,33 @@
 package blockchain;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+
 public class App {
+    // Creating an executor for creating many threads and assigning
+    // each thread a miner
     public static void main(String[] args) {
-        BlockChain blockchain = new BlockChain();
-        for (int counter = 0; counter < 5; counter++) {
-            blockchain.generateBlock();
+        BlockChain blockchain = BlockChainFileUtil.getOrCreateBlockChain();
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        for (int i = 0; i < 5; i++)
+            executor.execute(new Miner(blockchain));
+        executor.shutdown(); // Disable new tasks from being submitted
+        try {
+            // Wait a while for existing tasks to terminate
+            if (!executor.awaitTermination(60, TimeUnit.SECONDS)) {
+                executor.shutdownNow(); // Cancel currently executing tasks
+                // Wait a while for tasks to respond to being cancelled
+                if (!executor.awaitTermination(60, TimeUnit.SECONDS))
+                    System.err.println("Pool did not terminate");
+            }
+        } catch (InterruptedException ie) {
+            // (Re-)Cancel if current thread also interrupted
+            executor.shutdownNow();
+            // Preserve interrupt status
+            Thread.currentThread().interrupt();
         }
-        
-        for (var block : blockchain.blocks) {
-            System.out.println(block.toString() + "\n");
-        }
+        System.out.println(blockchain.toString());
     }
 }
